de00dcd2a4dab429422f7c35467debda499f2154
Fielstein0 <VHATVHFIELSE0@va.gov>
2018-05-22 16:22:10 -0500

    Add sites and date range json to store

--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     selectedSite: '',
-    selectedRange: ''
+    selectedRange: '',
+    siteNames: siteNames,
+    dateRange: dateRange
   },
   getters: {
     selectedSite (state) {

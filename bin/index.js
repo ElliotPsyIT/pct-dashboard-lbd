9ae78edcc96db891650b9bcda3eefa93722aa206
Fielstein0 <VHATVHFIELSE0@va.gov>
2019-02-08 11:26:14 -0600

    Clean move to production script

--- bin/index.js
+++ bin/index.js
@@ -17,21 +17,4 @@ console.log('overwrite dist\index.html to Production pct\index.html')
 fs.copySync("C:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\static", "C:\\PCT_Prod\\pct\\static")
 console.log('copy dist\static dir to Production \static directory')
 
-/*
-(async () => {
-	await trash(["C:\\PCT_Prod\\static"]);
-	console.log('delete static dir');
-	
-	await trash(["C:\\PCT_Prod\\index.html_"]);
-	console.log('deleted index.html_')
-	
-	await cpFile("C:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\index.html", "C:\\PCT_Prod\\index.html_");
-	console.log('copy index.html to index.html_')
-	
-	await cpFile("C:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\static\\*", "C:\\PCT_Prod\\static\\*");
-	console.log('copy static folder contents to prod')
-	
-})();
-*/
-
 

d0bf203c410673bc8515054a88bfddb31b701a2b
Fielstein0 <VHATVHFIELSE0@va.gov>
2020-02-28 11:37:10 -0600

    Bump the version to correct for linking issue

--- src/store/store.js
+++ src/store/store.js
@@ -109,7 +109,7 @@ const store = new Vuex.Store({
       survey: true,
       ebp: true,
     },
-    appVersion: '0.14.5',
+    appVersion: '0.14.6',
     phipii: 0,
     allphipii: [],
     adaccount: "",

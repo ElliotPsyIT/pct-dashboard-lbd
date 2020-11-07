399fa816b94909efabf3b14cae3a4448d85c3adb
Fielstein0 <VHATVHFIELSE0@va.gov>
2020-11-07 07:33:38 -0600

    Modify definitions and file formating

--- src/components/Dashboard/Documentation/ebp_doc.js
+++ src/components/Dashboard/Documentation/ebp_doc.js
@@ -1,7 +1,8 @@
 export const ebps = [
-    {
-      title: "Sessions Numerator",
-      value: "EBPs Session Counts<br/><br/>" + 
+  {
+    title: "Sessions Numerator",
+    value:
+      "EBPs Session Counts<br/><br/>" +
       "Numerator: The EBPs Session numerator reflects a count of PCT encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Only encounters of the following encounter types are used in counting EBP sessions:<br/><br/>" +
@@ -9,22 +10,24 @@ export const ebps = [
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "EBPs Defined ..."
-    },
-            {
-      title: "Sessions Denominator",
-      value: "EBPs Sessions Total Counts<br/><br/>" + 
+    category: "EBPs Defined ...",
+  },
+  {
+    title: "Sessions Denominator",
+    value:
+      "EBPs Sessions Total Counts<br/><br/>" +
       "Numerator: The EBPs Session denominator reflects a count of total PCT encounters whether documented as EBP sessions or not. <br/>" +
       "Only encounters of the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPT Procedure Codes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "EBPs Defined ..."
-    },
-    {
-      title: "Patients Numerator",
-      value: "EBPs Patient Counts<br/><br/>" + 
+    category: "EBPs Defined ...",
+  },
+  {
+    title: "Patients Numerator",
+    value:
+      "EBPs Patient Counts<br/><br/>" +
       "<u>Numerator</u>: The EBPs Patients numerator reflects a count of unique PCT patients with encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Patients are counted only if their encounter was from any of the following encounter types are included in counting EBP patients:<br/><br/>" +
@@ -32,25 +35,27 @@ export const ebps = [
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "EBPs Defined ..."
-    },
-    {
-      title: "Patients Denominator",
-      value: "EBPs Patients Total Counts<br/><br/>" + 
+    category: "EBPs Defined ...",
+  },
+  {
+    title: "Patients Denominator",
+    value:
+      "EBPs Patients Total Counts<br/><br/>" +
       "<u>Denominator</u>: The EBPs Patients denominator reflects a count of total unique PCT patients whether their sessions were documented as EBP sessions or not. <br/>" +
       "Patients are counted only if their encounter was from any the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPTCodes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "EBPs Defined ..."
-    }
-  ]
+    category: "EBPs Defined ...",
+  },
+];
 
-  export const PEorCPT  = [
-    {
-      title: "Session Counts: Numerator",
-      value: "PE or CPT Session Counts<br/><br/>" + 
+export const PEorCPT = [
+  {
+    title: "Session Counts: Numerator",
+    value:
+      "PE or CPT Session Counts<br/><br/>" +
       "<u>Numerator</u>: The PE or CPT Session numerator reflects a count of PCT encounters documented as EBP sessions, and" +
       "those EBP sessions must ALSO be designated as either PE (Prolonged Exposure) or CPT (Cogntive Processing Therapy)<br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation, and a PE or CPT Health Factor denoting the treatment type was detected." +
@@ -59,24 +64,24 @@ export const ebps = [
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "PEorCPT"
-    },
-    {
-      title: "Session Counts: Denominator",
-      value: "PE or CPT Session Counts<br/><br/>" + 
-      "<u>Denominator</u>: The PE or CPT Session denominator reflects a count of all PCT encounters documented as EBP sessions." +
-      "A documented EBP session is identified if an EBP template was used in its documentation.<br/>" +
-      "<b>NOTE</b>: This denominator of all PCT encounters documented as EBP sessions (no matter the EBP treatment type) is the same denominator as above for total EBPs Sessions denominator.<br/>" +
-      "Only encounters of the following encounter types are used in counting the total EBP sessions denominator:<br/><br/>" +
+    category: "PEorCPT",
+  },
+  {
+    title: "Session Counts: Denominator",
+    value:
+      "PE or CPT Session Counts<br/><br/>" +
+      "<u>Denominator</u>: The PE or CPT Session denominator reflects a count of total PCT encounters whether documented as EBP sessions or not.  " +
+      "Only encounters of the following encounter types are used in counting total denominator sessions (same Procedural Codes as numerator)<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPT Procedure Codes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "PEorCPT"
-    },
-    {
-      title: "Patient Counts: Numerator",
-      value: "PE or CPT Patient Counts<br/><br/>" + 
+    category: "PEorCPT",
+  },
+  {
+    title: "Patient Counts: Numerator",
+    value:
+      "PE or CPT Patient Counts<br/><br/>" +
       "<u>Numerator</u>: The PE or CPT Patients numerator reflects a count of unique PCT patients with encounters documented as EBP sessions. <br/>" +
       ", and those EBP sessions must ALSO be designated as either PE (Prolonged Exposure) or CPT (Cogntive Processing Therapy)<br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation, and a PE or CPT Health Factor denoting the treatment type was detected." +
@@ -85,28 +90,27 @@ export const ebps = [
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "PEorCPT"
-    },
-    {
-      title: "Patients Denominator",
-      value: "EBPs Patients Total Counts<br/><br/>" + 
-      "<u>Denominator</u>: The PE or CPT Patients denominator reflects a count of all PCT patient with EBP documented sessions." +
-      "A documented EBP session is identified if an EBP template was used in its documentation.<br/>" +
-      "<b>NOTE</b>: This denominator of all PCT patients with documented EBP sessions (no matter the EBP treatment type - PE, CPT or other) is the same denominator as above for total PE CPT Sessions denominator.<br/>" +
-      "Only encounters of the following encounter types are used in counting the total EBP sessions denominator:<br/><br/>" +
+    category: "PEorCPT",
+  },
+  {
+    title: "Patients Denominator",
+    value:
+      "EBPs Patients Total Counts<br/><br/>" +
+      "<u>Denominator</u>: The PE or CPT Patients denominator reflects a count of total unique PCT patients whether their sessions were documented as EBP sessions or not.  " +
+      "Patients are counted only if their encounter was from any of the following encounter types used in counting the total denominator sessions (same Procedural Codes as numerator):<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPT Procedure Codes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>" +
       "FAMILY SESSIONS: CPT Procedure Codes 90847 OR 90849",
-      category: "PEorCPT"
-    }
+    category: "PEorCPT",
+  },
+];
 
-  ]
-
-  export const IndividualAndGroupTxEBPs = [
-    {
-      title: "Individual Sessions",
-      value: "Individual Tx Session Counts<br/><br/>" + 
+export const IndividualAndGroupTxEBPs = [
+  {
+    title: "Individual Sessions",
+    value:
+      "Individual Tx Session Counts<br/><br/>" +
       "<u><b>Numerator</b></u>: The Individual Tx Session numerator reflects a count of PCT encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Only encounters of the following encounter types are used in counting EBP Individual Tx sessions:<br/><br/>" +
@@ -116,11 +120,12 @@ export const ebps = [
       "Only encounters of the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPT Procedure Codes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>",
-      category: "Individual & Group Tx EBPs Defined ..."
-    },
-     {
-      title: "Individual Patients",
-      value: "Individual Tx EBPs Patient Counts<br/><br/>" + 
+    category: "Individual & Group Tx EBPs Defined ...",
+  },
+  {
+    title: "Individual Patients",
+    value:
+      "Individual Tx EBPs Patient Counts<br/><br/>" +
       "<u><b>Numerator</b></u>: The EBPs Individual Tx Patients numerator reflects a count of unique PCT patients with encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Patients are counted only if their encounter was from any of the following encounter types are included in counting EBP patients:<br/><br/>" +
@@ -130,11 +135,12 @@ export const ebps = [
       "Patients are counted only if their encounter was from any the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "INDIVIDUAL SESSIONS: CPT Procedure Codes 90832 OR 90834 OR 90837<br/>" +
       "CRISIS SESSIONS: CPT Procedure Codes 90839<br/>",
-      category: "Individual & Group Tx EBPs Defined ..."
-    },
-    {
-      title: "Group Sessions",
-      value: "Group Tx Session Counts<br/><br/>" + 
+    category: "Individual & Group Tx EBPs Defined ...",
+  },
+  {
+    title: "Group Sessions",
+    value:
+      "Group Tx Session Counts<br/><br/>" +
       "<u><b>Numerator</b></u>: The Individual Tx Session numerator reflects a count of PCT encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Only encounters of the following encounter types are used in counting EBP Individual Tx sessions:<br/><br/>" +
@@ -142,11 +148,12 @@ export const ebps = [
       "<u><b>Denominator</b></u>: The EBPs Session denominator reflects a count of total PCT encounters whether documented as EBP sessions or not. <br/>" +
       "Only encounters of the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>",
-      category: "Individual & Group Tx EBPs Defined ..."
-    },
-     {
-      title: "Group Patients",
-      value: "Group Tx EBPs Patient Counts<br/><br/>" + 
+    category: "Individual & Group Tx EBPs Defined ...",
+  },
+  {
+    title: "Group Patients",
+    value:
+      "Group Tx EBPs Patient Counts<br/><br/>" +
       "<u><b>Numerator</b></u>: The EBPs Individual Tx Patients numerator reflects a count of unique PCT patients with encounters documented as EBP sessions. <br/>" +
       "A documented EBP session is identified if an EBP template was used in its documentation." +
       "Patients are counted only if their encounter was from any of the following encounter types are included in counting EBP patients:<br/><br/>" +
@@ -154,31 +161,34 @@ export const ebps = [
       "<u><b>Denominator</b></u>: The EBPs Patients denominator reflects a count of total unique PCT patients whether their sessions were documented as EBP sessions or not. <br/>" +
       "Patients are counted only if their encounter was from any the following encounter types are used in counting total denominator sessions (same CPT Codes are numerator):<br/><br/>" +
       "GROUP SESSIONS: CPT Procedure Codes 90853<br/>",
-      category: "Individual & Group Tx EBPs Defined ..."
-    },
-  ]
+    category: "Individual & Group Tx EBPs Defined ...",
+  },
+];
 
-  export const ebpActivitySummary = [
-    {
-      title: "EBP Patient Counts Table (left)",
-      value: "These are a list of the number of unique patients administered EBPs by EBP Types at each institution of a medical center site.<br/><br/>" +
+export const ebpActivitySummary = [
+  {
+    title: "EBP Patient Counts Table (left)",
+    value:
+      "These are a list of the number of unique patients administered EBPs by EBP Types at each institution of a medical center site.<br/><br/>" +
       "",
-      category: "EBP Activity Summary Defined ..."
-    },
-    {
-      title: "EBP Types Pie Chart (right)",
-      value: "The EBP Types Pie Chart displays the relative proportions, with counts, of the EBP types administerd at the selected medical center site.<br/>",
-      category: "EBP Activity Summary Defined ..."
-    }
-  ]
+    category: "EBP Activity Summary Defined ...",
+  },
+  {
+    title: "EBP Types Pie Chart (right)",
+    value:
+      "The EBP Types Pie Chart displays the relative proportions, with counts, of the EBP types administerd at the selected medical center site.<br/>",
+    category: "EBP Activity Summary Defined ...",
+  },
+];
 
-  export const ProvidersEBPsAndSurveysByClinicAndPatient = [
-    {
-      title: "Provider's EBPs and Surveys By Clinic and Patient Table",
-      value: "The Provider's EBPs and Surveys By Clinic and Patient Table displays patient level EBP sessions and any administered surveys for each patient by provider at each institution of the selected medical center site. <br/><br/>" + 
+export const ProvidersEBPsAndSurveysByClinicAndPatient = [
+  {
+    title: "Provider's EBPs and Surveys By Clinic and Patient Table",
+    value:
+      "The Provider's EBPs and Surveys By Clinic and Patient Table displays patient level EBP sessions and any administered surveys for each patient by provider at each institution of the selected medical center site. <br/><br/>" +
       "The association of EBP sesions and same day survey administration represents a Measurement Based Care proxy measure, showing the use of data in the context of EBPs. <br/><br/>" +
       "<u><b>EBP Sessions</b></u>: Each provider's EBP sessions are shown including the patient initials and last 4, EBP type, and session date<br/><br/>" +
       "<u><b>Surveys Administered</b></u>: Any surveys administered on the same day as the EBP session are listed next to the patient/session information.  Survey name, score, and date of administration are listed.",
-      category: "Provider's EBPs By Clinic and Patient Defined ..."
-    }
-  ]
+    category: "Provider's EBPs By Clinic and Patient Defined ...",
+  },
+];

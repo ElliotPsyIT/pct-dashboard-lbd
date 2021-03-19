6636e450b06e8907ab6024000b87d4a57deb6e56
Elliot Fielstein <elliot.fielstein@va.gov>
2021-03-19 13:22:23 -0500

    Add MBC widgets documentation

--- src/components/Dashboard/Documentation/surveys_doc.js
+++ src/components/Dashboard/Documentation/surveys_doc.js
@@ -1,3 +1,54 @@
+export const PCL5Administrations = [
+  {
+    title: "PCL5 Administrations",
+    value:
+      "All PCL-5 Listings Include Both PCL5 Weekly and PCL5 Monthly<br/><br/> " +
+      "<u><b>Description</b></u>: PCL-5s administered in all defined PCT Clinics are displayed for the selected date range and from the selected site. PCT Clinics at the medical center are set up within VistA configured using one more stop codes.<br/><br/> " + 
+      "<u><b>Stop Codes</b></u>: To identify PCL-5 Administrations from PCT Clinics, we use the following PTSD stop codes when they are in the PRIMARY position only.<br/><br/>" +
+      "<u><b>516 PTSD - Group</b></u>: Records consultation and/or treatment follow-up provided to more than one individual. Treatment is provided to those patients with PTSD. Includes provider and support services. <br/><br/> " +
+      "<u><b>542 PTSD Telephone PTSD</b></u>: Records patient consultation or medical care management, advice, and/or referral provided by telephone contact between patient or patient's next-of-kin and/or the person(s) with whom the patient has a meaningful relationship, and clinical and/or professional staff assigned to the PCT.<br/><br/> " +
+      "<u><b>562 PTSD - Individual</b></u>: Records patient visit for consultation, evaluation, follow-up, and/or treatment provided to an individual with PTSD. Includes provider and support services. ",
+    category: "PCL5 Administrations Defineed...",
+  },
+];
+
+export const PCL5AdministrationsAndPatientEncounters = [
+  {
+    title: "All Patients",
+    value:
+      "<u><b>ALL Patients</b></u>: The numerator includes unique patients seen in a  PTSD clinic (based on primary PTSD stop codes) who have been administered at least one PCL5 Weekly/Monthly for the selected date range and from the selected site.  The denominator includes all unique patients seen in a PTSD clinic (based on primary PTSD stop codes) for the selected date range and from the selected site.",
+    category: "Patients PCL5s Defined...",
+  },
+  {
+    title: "Patients Seen 2 or More Times",
+    value:
+      "<u><b>Patients Seen 2 or More Times</b></u>: The numerator includes unique patients seen in a PTSD clinic (based on primary PTSD stop codes) who have been administered 2 or more PCL5 Weekly/Monthly for the selected date range and from the selected site.  The denominator includes all unique patients seen <b>at least twice</b> in a PTSD clinic (based on primary PTSD stop codes) for the selected date range and from the selected site.",
+    category: "Patients PCL5s Defined...",
+  },
+
+];
+
+export const PCL5AdministrationsAndProviders= [
+  {
+    title: "Encounter Providers PCL5s",
+    value:
+      "<u><b>Encounter Providers PCL5s</b></u>: The numerator includes number of unique providers selected as primary providers in a PTSD clinic (based on primary PTSD Stop Codes) who have administered at least one PCL5 Weekly/Monthly for the selected date range and from the selected site.  The denominator includes number of providers selected as primary providers in a PTSD clinic (based on PTSD Stop Codes) for the selected date range and from the selected site. ",
+    category: "Providers Defined...",
+  },
+  
+];
+
+export const PCL5ProviderPatientPanels = [
+  {
+    title: "PCL5 Providers PCL5 Patient Panels",
+    value:
+      "<u><b>Description</b></u>: These are the total number of surveys (measures) administered.  Only completed surveys are tallied and displayed here.",
+    category: "Proivder Patient Panels Defined...",
+  },
+
+];
+
+
 export const SurveysSummary = [
   {
     title: "Surveys Summary",
@@ -54,3 +105,5 @@ export const SurveysToPatientsByProvider = [
     category: "Surveys To Patients By Provider Defined...",
   },
 ];
+
+

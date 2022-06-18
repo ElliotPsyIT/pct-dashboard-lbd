e37584d1406953be6218f2e1eb0e981cfe8aea8e
Elliot M Fielstein <elliot.fielstein@va.gov>
2022-06-18 17:33:50 -0500

    Create documentation file for new TLC page

---
+++ src/components/Dashboard/Documentation/time_limited_care.js
@@ -0,0 +1,41 @@
+export const TimeLimitedCare6and20 = [
+  {
+    title: 'TLC 20 Sessions in 6 Months',
+    value: '<u><b>Numerator</b></u>: Total Patients from all PCT Clinics who have been seen for Individual or Group Therapy within the last 3 months who ' +
+    'and who have been seen for at least 20 sessions over the last 6 months:<br/><br/>' +
+    '&nbsp;&nbsp;&nbsp; <u><b>Individual Therapy CPT Codes</b></u>: (incl. 1 Crisis Code AND 2 Family Codes): <br/>' +
+    "('90832', '90834', '90837', '90839', '90847', '90849')<br/><br/>" +
+    '<u><b>Denominator</b></u>: Total Patients from all PCT Clinics seen for Individual or Group Therapy in the last 3 months.<br/><br/>',
+    category: 'Numerous Session Cohort Defined...'
+  }
+]
+
+export const TimeLimitedCare12and12 = [
+  {
+    title: 'TLC 12 Sessions in 12 Months',
+    value: '<u><b>Numerator</b></u>: Total Patients from all PCT Clinics who have been seen for Individual or Group Therapy within the last 3 months who ' +
+    'and who have been seen for at least 20 sessions over the last 6 months:<br/><br/>' +
+    '&nbsp;&nbsp;&nbsp; <u><b>Individual Therapy</b></u>: (incl. 1 Crisis Code AND 2 Family Codes): <br/>' +
+    "('90832', '90834', '90837', '90839', '90847', '90849')<br/><br/>" +
+    '<u><b>Denominator</b></u>: Total Patients from all PCT Clinics seen for Individual or Group Therapy in the last 3 months.<br/><br/>',
+    category: 'Lengthy Treatment Cohort Defined...'
+  }
+]
+
+export const TimeLimitedCare6and20Table = [
+  {
+    title: 'TLC 6 and 20 Activity Summary',
+    value: '<u><b>Description</b></u>: Provides each TLC patient\'s treatment activities within the period of 20 sessions occurring' +
+      'over 6 months with the most recent session within 3 months to the current date. ',
+    category: 'Numerous Session Activity Summary Defined...'
+  }
+]
+
+export const TimeLimitedCare12and12Table = [
+  {
+    title: 'TLC 12 and 12 Activity Summary',
+    value: '<u><b>Description</b></u>: Provides each TLC patient\'s treatment activities within the period of 12 sessions occurring' +
+    'over 12 months with the most recent session within 3 months to the current date. ',
+    category: 'Lengthy Treatment Activity Summary Defined...'
+  }
+]

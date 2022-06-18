e1bf3d304f43f3b55ed977038d0e5a882c6cbf0c
Elliot M Fielstein <elliot.fielstein@va.gov>
2022-06-18 17:29:15 -0500

    Modify pointers to bin dev and index.js to D:

--- bin/dev.js
+++ bin/dev.js
@@ -8,15 +8,15 @@
 const fs = require('fs-extra')
 
 // remove dir
-fs.removeSync("C:\\PCT_Prod\\dev\\static")
+fs.removeSync("D:\\PCT_Prod\\dev\\static")
 console.log('remove Development \static directory')
 
 // copy file and overwrite
-fs.copySync("C:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\index.html", "C:\\PCT_Prod\\dev\\index.html", { overwrite: true })
+fs.copySync("D:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\index.html", "D:\\PCT_Prod\\dev\\index.html", { overwrite: true })
 console.log('overwrite dist\index.html to Development dev\index.html')
 
 // copy dir
-fs.copySync("C:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\static", "C:\\PCT_Prod\\dev\\static")
+fs.copySync("D:\\PCT_Admin\\pct_admin_app\\vue-light-bootstrap-dashboard\\dist\\static", "D:\\PCT_Prod\\dev\\static")
 console.log('copy dist\static dir to Development \static directory')
 
 

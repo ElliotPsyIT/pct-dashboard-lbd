85b47394db9cc4ea29590919187285f7d1572dd4
Elliot M Fielstein <elliot.fielstein@va.gov>
2022-06-18 17:45:44 -0500

    Add route for new tcl page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,57 +1,59 @@
-import DashboardLayout from "../components/Dashboard/Layout/DashboardLayout.vue";
+import DashboardLayout from '../components/Dashboard/Layout/DashboardLayout.vue'
 // GeneralViews
-import NotFound from "../components/GeneralViews/NotFoundPage.vue";
+import NotFound from '../components/GeneralViews/NotFoundPage.vue'
 
 // Admin pages
-import Overview from "src/components/Dashboard/Views/Overview.vue";
-import Appointments from "src/components/Dashboard/Views/Appointments.vue";
-import Consults from "src/components/Dashboard/Views/Consults.vue";
-import Encounters from "src/components/Dashboard/Views/Encounters.vue";
-import Providers from "src/components/Dashboard/Views/Providers.vue";
+import Overview from 'src/components/Dashboard/Views/Overview.vue'
+import Appointments from 'src/components/Dashboard/Views/Appointments.vue'
+import Consults from 'src/components/Dashboard/Views/Consults.vue'
+import Encounters from 'src/components/Dashboard/Views/Encounters.vue'
+import Providers from 'src/components/Dashboard/Views/Providers.vue'
 // import Surveys from "src/components/Dashboard/Views/Surveys.vue";
-import MBC from "src/components/Dashboard/Views/MBC.vue";
-import EBP from "src/components/Dashboard/Views/EBP.vue";
-import Explore from "src/components/Dashboard/Views/Explore.vue";
+import MBC from 'src/components/Dashboard/Views/MBC.vue'
+import EBP from 'src/components/Dashboard/Views/EBP.vue'
+import Explore from 'src/components/Dashboard/Views/Explore.vue'
 
-import Definitions from "src/components/Dashboard/Views/DataDefinitions.vue";
+import TLC from 'src/components/Dashboard/Views/TLC.vue'
+
+// import Definitions from 'src/components/Dashboard/Views/DataDefinitions.vue'
 
 // import Icons from 'src/components/Dashboard/Views/Icons.vue'
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     component: DashboardLayout,
-    redirect: "/admin/overview",
+    redirect: '/admin/overview'
   },
   {
-    path: "/admin",
+    path: '/admin',
     component: DashboardLayout,
-    redirect: "/admin/overview",
+    redirect: '/admin/overview',
     children: [
       {
-        path: "overview",
-        name: "Overview",
-        component: Overview,
+        path: 'overview',
+        name: 'Overview',
+        component: Overview
       },
       {
-        path: "consults",
-        name: "Consults",
-        component: Consults,
+        path: 'consults',
+        name: 'Consults',
+        component: Consults
       },
       {
-        path: "appointments",
-        name: "Cancel No Show",
-        component: Appointments,
+        path: 'appointments',
+        name: 'Cancel No Show',
+        component: Appointments
       },
       {
-        path: "encounters",
-        name: "Encounters",
-        component: Encounters,
+        path: 'encounters',
+        name: 'Encounters',
+        component: Encounters
       },
       {
-        path: "providers",
-        name: "Providers",
-        component: Providers,
+        path: 'providers',
+        name: 'Providers',
+        component: Providers
       },
       // {
       //   path: "surveys",
@@ -59,29 +61,34 @@ const routes = [
       //   component: Surveys,
       // },
       {
-        path: "mbc",
-        name: "MBC",
-        component: MBC,
+        path: 'mbc',
+        name: 'MBC',
+        component: MBC
       },
       {
-        path: "ebp",
-        name: "EBP",
-        component: EBP,
+        path: 'ebp',
+        name: 'EBP',
+        component: EBP
       },
       {
-        path: "explore",
-        name: "Explore",
-        component: Explore,
+        path: 'tlc',
+        name: 'TLC',
+        component: TLC
       },
+      {
+        path: 'explore',
+        name: 'Explore',
+        component: Explore
+      }
       // {
       //   path: 'definitions',
       //   name: 'Definitions',
       //   component: Definitions
       // }
-    ],
+    ]
   },
-  { path: "*", component: NotFound },
-];
+  { path: '*', component: NotFound }
+]
 
 /**
  * Asynchronously load view (Webpack Lazy loading compatible)
@@ -93,4 +100,4 @@ const routes = [
 //    return res;
 // };
 
-export default routes;
+export default routes
